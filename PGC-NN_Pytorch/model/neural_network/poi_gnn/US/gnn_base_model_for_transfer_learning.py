68a8ef140cd47bf8a112ed50ddf4aa629bc25a6c
TarikSalles <114872359+TarikSalles@users.noreply.github.com>
2024-04-19 12:40:09 -0300

    Mudanças no modelo (adicionado camada Relu e outras alterações)

--- PGC-NN_Pytorch/model/neural_network/poi_gnn/US/gnn_base_model_for_transfer_learning.py
+++ PGC-NN_Pytorch/model/neural_network/poi_gnn/US/gnn_base_model_for_transfer_learning.py
@@ -49,31 +49,34 @@ class GNNUS_BaseModel(nn.Module):
     def forward(self, A_input, A_week_input, A_weekend_input, Temporal_input, Temporal_week_input,
                 Temporal_weekend_input, Distance_input, Duration_input, Location_time_input, Location_location_input):
 
+        elu_activation_arma_conv = nn.ELU()  # Objeto de ativação ELU
+        softmax_activation_arma_conv = nn.Softmax()  # Objeto de ativação Softmax
+
+
         A_input = A_input.view(2, -1) #Matrizes de adjacência devem possuir formato (2,E) na ARMAConv
         A_input = A_input.to(torch.int64) #Matrizes de adjacência devem ser somente inteiras
 
         Temporal_input = Temporal_input.to(torch.float32)
         Temporal_input = Temporal_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
-        elu_activation = nn.ELU() #Objeto de ativação ELU
-        softmax_activation = nn.Softmax() #Objeto de ativação Softmax
 
-        out_temporal = elu_activation(self.out_temporal(Temporal_input,A_input)) #Ativação ELU chamada
+
+        out_temporal = elu_activation_arma_conv(self.out_temporal(Temporal_input,A_input)) #Ativação ELU chamada
         out_temporal = F.dropout(out_temporal, p=0.3) #Dropout
         arma_out_temporal = ARMAConv(in_channels=-1, out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip) #Segunda camada ARMAConv sem ativação distinta
         out_temporal = out_temporal.to(torch.float32)
-        out_temporal = softmax_activation(arma_out_temporal(out_temporal,A_input)) #Ativação Sofmax chamada
+        out_temporal = softmax_activation_arma_conv(arma_out_temporal(out_temporal,A_input)) #Ativação Sofmax chamada
 
         A_week_input = A_week_input.view(2, -1) #Matrizes de adjacência devem possuir formato (2,E) na ARMAConv
         A_week_input = A_week_input.to(torch.int64) #Matrizes de adjacência devem ser somente inteiras
         Temporal_week_input = Temporal_week_input.to(torch.float32)
         Temporal_week_input = Temporal_week_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
-        out_week_temporal = elu_activation(self.out_week_temporal(Temporal_week_input, A_week_input)) #Ativação ELU chamada
+        out_week_temporal = elu_activation_arma_conv(self.out_week_temporal(Temporal_week_input, A_week_input)) #Ativação ELU chamada
         out_week_temporal = F.dropout(out_week_temporal, p=0.3) #Dropout
         arma_out_week_temporal = ARMAConv(in_channels=-1,out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip) #Segunda camada ARMAConv sem ativação distinta
         out_week_temporal = out_week_temporal.to(torch.float32)
-        out_week_temporal = softmax_activation(arma_out_week_temporal(out_week_temporal,A_week_input)) #Ativação Sofmax chamada
+        out_week_temporal = softmax_activation_arma_conv(arma_out_week_temporal(out_week_temporal,A_week_input)) #Ativação Sofmax chamada
 
         A_weekend_input = A_weekend_input.view(2, -1) #Matrizes de adjacência devem possuir formato (2,E) na ARMAConv
         A_weekend_input = A_weekend_input.to(torch.int64) #Matrizes de adjacência devem ser somente inteiras
@@ -81,32 +84,32 @@ class GNNUS_BaseModel(nn.Module):
         Temporal_weekend_input = Temporal_weekend_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
 
-        out_weekend_temporal = elu_activation(self.out_weekend_temporal(Temporal_weekend_input, A_weekend_input)) #Ativação ELU chamada
+        out_weekend_temporal = elu_activation_arma_conv(self.out_weekend_temporal(Temporal_weekend_input, A_weekend_input)) #Ativação ELU chamada
         out_weekend_temporal = F.dropout(out_weekend_temporal, p=0.3) #Dropout
         arma_out_weekend_temporal = ARMAConv(in_channels=-1,out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip)  #Segunda camada ARMAConv sem ativação distinta
         out_weekend_temporal = out_weekend_temporal.to(torch.float32)
-        out_weekend_temporal = softmax_activation(arma_out_weekend_temporal(out_weekend_temporal,A_weekend_input)) #Ativação Softmax chamada
+        out_weekend_temporal = softmax_activation_arma_conv(arma_out_weekend_temporal(out_weekend_temporal,A_weekend_input)) #Ativação Softmax chamada
 
         Distance_input = Distance_input.to(torch.float32)
         Distance_input = Distance_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
 
-        out_distance = elu_activation(self.out_distance(Distance_input, A_input)) #Ativação ELU chamada
+        out_distance = elu_activation_arma_conv(self.out_distance(Distance_input, A_input)) #Ativação ELU chamada
         out_distance = F.dropout(out_distance, p=0.3) #Dropout
         arma_out_distance = ARMAConv(in_channels=-1,out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip)  #Segunda camada ARMAConv sem ativação distinta
         out_distance = out_distance.to(torch.float32)
-        out_distance = softmax_activation(arma_out_distance(out_distance, A_input)) #Ativação Softmax chamada
+        out_distance = softmax_activation_arma_conv(arma_out_distance(out_distance, A_input)) #Ativação Softmax chamada
 
         Duration_input = Duration_input.to(torch.float32)
         Duration_input = Duration_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
 
-        out_duration = elu_activation(self.out_duration(Duration_input, A_input)) #Ativação ELU chamada
+        out_duration = elu_activation_arma_conv(self.out_duration(Duration_input, A_input)) #Ativação ELU chamada
         out_duration = F.dropout(out_duration, p=0.3) #Dropout
         arma_out_duration = ARMAConv(in_channels=-1,out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip) #Segunda camada ARMAConv sem ativação distinta
 
         out_duration = out_duration.to(torch.float32)
-        out_duration = softmax_activation(arma_out_duration(out_duration,A_input)) #Ativação Softmax chamada
+        out_duration = softmax_activation_arma_conv(arma_out_duration(out_duration,A_input)) #Ativação Softmax chamada
 
         Location_location_input = Location_location_input.view(2, -1) #Matrizes de adjacência devem possuir formato (2,E) na ARMAConv
         Location_location_input = Location_location_input.to(torch.int64) #Matrizes de adjacência devem ser somente inteiras
@@ -114,28 +117,31 @@ class GNNUS_BaseModel(nn.Module):
         Location_time_input = Location_time_input.permute(1, 0, 2) #Permutação da matriz pois a ARMAConv requer que o numero de vertices venha primeiro
 
 
-        out_location_location = elu_activation(self.out_location_location(Location_time_input, Location_location_input)) #Ativação ELU chamada
+        out_location_location = elu_activation_arma_conv(self.out_location_location(Location_time_input, Location_location_input)) #Ativação ELU chamada
         out_location_location = F.dropout(out_location_location, p=0.3) #Dropout
         arma_out_location_location = ARMAConv(in_channels=-1,out_channels=self.classes,shared_weights=share_weights, dropout=dropout_skip) #Segunda camada ARMAConv sem ativação distinta
 
         out_location_location = out_location_location.to(torch.float32)
-        out_location_location = softmax_activation(arma_out_location_location(out_location_location,Location_location_input))  #Ativação Softmax chamada
+        out_location_location = softmax_activation_arma_conv(arma_out_location_location(out_location_location,Location_location_input))  #Ativação Softmax chamada
 
         out_location_time = self.out_location_time(Location_time_input) #Camada linear
-        out_location_time = F.dropout(out_location_time, p=0.3) #Dropout
-        Linear_layer = nn.Linear(40, self.classes) #Camada linear
 
-        out_location_time = softmax_activation(Linear_layer(out_location_time))  #Ativação Softmax chamada
+        out_location_time = F.relu(out_location_time)  #Linha adicionada 09/04
+
+        out_location_time = F.dropout(out_location_time, p=0.3) #Dropout
+        Linear_Layer_Dense_Out_Location = nn.Linear(40, self.classes) #Camada linear
+
+        out_location_time = softmax_activation_arma_conv(Linear_Layer_Dense_Out_Location(out_location_time))  #Ativação Softmax chamada
 
 
         out_dense = 2.0 * out_location_location + 2.0 * out_location_time #Multiplicação com pesos
-        out_dense = softmax_activation(self.out_dense(out_dense))  #Ativação Softmax chamada
+        out_dense = softmax_activation_arma_conv(self.out_dense(out_dense))  #Ativação Softmax chamada
 
         out_gnn = 1.0 * out_temporal + 1.0 * out_week_temporal + 1.0 * out_weekend_temporal + 1.0 * out_distance + 1.0 * out_duration #Multiplicação com pesos
-        out_gnn = softmax_activation(self.out_gnn(out_gnn))  #Ativação Softmax chamada
+        out_gnn = softmax_activation_arma_conv(self.out_gnn(out_gnn))  #Ativação Softmax chamada
 
 
         out = 1.0 * out_dense + 1.0 * out_gnn #Multiplicação com pesos
         out = out.permute(1, 2, 0)  ##Permutação da matriz tridimensional out afim de seguir o formato requisitada
 
-        return  out #Retorno da camada final
+        return out #Retorno da camada final
