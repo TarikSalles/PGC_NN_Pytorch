5d0a001bb917547c78b2191cf4012bfb0d9a9ad0
TarikSalles <114872359+TarikSalles@users.noreply.github.com>
2024-06-07 11:20:36 -0300

    Add files via upload

---
+++ PGC-NN_Pytorch/README.md
@@ -0,0 +1,57 @@
+# PGC-NN
+
+## Description
+
+This repository presents PGC-NN: a graph neural network able to semantically enriching Points of Interest (POIs) according to spatiotemporal data.
+
+This approach is a piece of a more complete work [1], removed from the original repository [2].
+
+## Data
+
+To generate the matrices specified in [1], see details in [matrix generation documentation](docs/matrix_generation.md).
+
+<span style="color: red;">**OBS: The Data was removed to not overload the repository.**</span>
+
+## Requirements and Execution
+
+### Requirements
+
+* Python 3
+    * [link](https://www.python.org/downloads/)
+* virtualenv
+    * `$ pip install virtualenv`
+
+### Execution
+
+After generate the matrices, you need to run 'processing_data.ipynb' notebook in 'gowalla/processed/' folder to reduce the original dataset and favor tests execution. You will need also to comment line 12 and uncomment line 11 in 'main.py' file.
+
+To execute this project, first you need to create and activate your virtual environment. The step-by-step is:
+
+<span style="color: red;">**OBS: The follow step-by-step was created to use in Linux Operation System.**</span>
+
+1. Create your virtual environment
+    * `$ virtualenv myenv`
+    * Replace 'myenv' with a name of your choice
+    * Execute this command only at the first time
+
+2. Activate the virtual environment
+    * `$ source myenv/bin/activate`
+
+3. Update pip
+    * `$ pip install --upgrade pip`
+
+4. Install requirements
+    * `$ pip install -r requirements.txt`
+5. Execute
+    * `$ python main.py`
+    * see [info](docs/info.md) about pgc-nn execution (pt-br)
+
+***
+
+## References
+
+[1] Cláudio G.S. Capanema, et al. Combining recurrent and Graph Neural Networks to predict the next place’s category. Ad Hoc Netw. 138, C (Jan 2023). https://doi.org/10.1016/j.adhoc.2022.103016
+
+[2] CLAUDIOCAPANEMA. poi_gnn. Disponível em: <https://github.com/claudiocapanema/poi_gnn>.
+
+***
--- PGC-NN_Pytorch/main.py
+++ PGC-NN_Pytorch/main.py
@@ -1,63 +1,39 @@
 import os
 import warnings
-import argparse
-
 warnings.filterwarnings("ignore")
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from job.poi_categorization_job import PoiCategorizationJob
 from job.matrix_generation_for_poi_categorization_job import MatrixGenerationForPoiCategorizationJob
 
+def exibir_menu():
+    opcoes = ["Executar", "Gerar entradas", "Sair"]
+    
+    while True:
+        print("-" * 27)
+        print("|          PGC-NN         |")
+        print("-" * 27)
+        
+        for i, opcao in enumerate(opcoes, start=1):
+            print(f"| {i}. {opcao:<20} |")
+        
+        print("-" * 27)
+        
+        escolha = input("Escolha uma opção (1-3): ")
 
-def execute_poi_categorization():
-    print("Você escolheu executar o PGC-NN")
-    job = PoiCategorizationJob()
-    job.start()
-
-
-def generate_matrix_for_poi_categorization():
-    print("Você escolheu gerar as entradas para o PGC-NN")
-    job = MatrixGenerationForPoiCategorizationJob()
-    job.start()
-
-
-def exibir_menu(args):
-    if args.execute:
-        execute_poi_categorization()
-    elif args.generate:
-        generate_matrix_for_poi_categorization()
-    else:
-        opcoes = ["Executar", "Gerar entradas", "Sair"]
-        while True:
-            print("-" * 27)
-            print("|          PGC-NN         |")
-            print("-" * 27)
-
-            for i, opcao in enumerate(opcoes, start=1):
-                print(f"| {i}. {opcao:<20} |")
-
-            print("-" * 27)
-
-            escolha = input("Escolha uma opção (1-3): ")
-
-            if escolha == "1":
-                execute_poi_categorization()
-            elif escolha == "2":
-                generate_matrix_for_poi_categorization()
-            elif escolha == "3":
-                print("Saindo do programa!")
-                break
-            else:
-                print("Opção inválida. Tente novamente.")
-
-
-def main():
-    parser = argparse.ArgumentParser(description="PGC-NN Command Line Interface")
-    parser.add_argument('--execute', action='store_true', help='Execute the Poi Categorization Job directly')
-    parser.add_argument('--generate', action='store_true', help='Generate Matrix for Poi Categorization Job directly')
-    args = parser.parse_args()
-    exibir_menu(args)
-
+        if escolha == "1":
+            print("Você escolheu executar o PGC-NN")
+            job = PoiCategorizationJob()
+            job.start()
+        elif escolha == "2":
+            print("Você escolheu gerar as entradas para o PGC-NN")
+            job = MatrixGenerationForPoiCategorizationJob()
+            job.start()
+        elif escolha == "3":
+            print("Saindo do programa!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
-    main()
+    exibir_menu()
